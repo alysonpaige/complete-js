b1769634388e9cb637766ef73af782a0a58fd9c8
Alyson Stiffel <alysonstiffel@gmail.com>
2018-01-04 16:07:37 -0500

    variable mutation and type coercion

--- JS Language Basics/script.js
+++ JS Language Basics/script.js
@@ -1,12 +1,38 @@
-// Variables and data types
+// Variables and data types:
+// var name = 'John';
+// console.log(name);
+
+// var lastName = 'Smith';
+// console.log(lastName);
+
+// var age = 26;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// Variable mutation and type coercion:
 var name = 'John';
-console.log(name);
-
-var lastName = 'Smith';
-console.log(lastName);
-
 var age = 26;
-console.log(age);
 
-var fullAge = true;
-console.log(fullAge);
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
