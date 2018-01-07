3cf69dce945e5056314611fde68eec468b12c60a
Alyson Stiffel <alysonstiffel@gmail.com>
2018-01-07 09:55:32 -0500

    statements and expressions

--- JS Language Basics/script.js
+++ JS Language Basics/script.js
@@ -120,26 +120,44 @@
 // }
 
 // Functions:
-function calculateAge(yearOfBirth) {
-  var age = 2018 - yearOfBirth;
-  return age;
+// function calculateAge(yearOfBirth) {
+//   var age = 2018 - yearOfBirth;
+//   return age;
+// }
+
+// var ageJohn = calculateAge(1990);
+// var ageMike = calculateAge(1969);
+// var ageMary = calculateAge(1948);
+
+// function yearsUntilRetirement(name, year) {
+//   var age = calculateAge(year);
+//   var retirement = 65 - age;
+
+//   if(retirement >= 0) {
+//     console.log(name + ' retires in ' + retirement + ' years.');
+//   } else {
+//     console.log(name + ' is already retired.');
+//   }
+// }
+
+// yearsUntilRetirement('John', 1990);
+// yearsUntilRetirement('Mike', 1969);
+// yearsUntilRetirement('Mary', 1948);
+
+// Statements and expressions
+// Statement (action):
+function someFun(par) {
+  // code
 }
 
-var ageJohn = calculateAge(1990);
-var ageMike = calculateAge(1969);
-var ageMary = calculateAge(1948);
-
-function yearsUntilRetirement(name, year) {
-  var age = calculateAge(year);
-  var retirement = 65 - age;
-
-  if(retirement >= 0) {
-    console.log(name + ' retires in ' + retirement + ' years.');
-  } else {
-    console.log(name + ' is already retired.');
-  }
+if (x === 5) {
+  // do something
 }
 
-yearsUntilRetirement('John', 1990);
-yearsUntilRetirement('Mike', 1969);
-yearsUntilRetirement('Mary', 1948);
+// Expression (value, outcome):
+var someFun = function(par) {
+  // code
+}
+
+3 + 4;
+var x = 3;
