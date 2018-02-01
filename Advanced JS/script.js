6ab9389f6a8c3cd105d971a13c542dfe30fcbfe4
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-01 17:59:10 -0500

    creating objects

--- Advanced JS/script.js
+++ Advanced JS/script.js
@@ -1,4 +1,4 @@
-// Function Constructors
+// Function Constructors:
 
 var john = {
   name: 'John',
@@ -29,3 +29,23 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+
+// Creating Objects:
+var personProto = {
+  calculateAge: function() {
+    console.log(2018 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, 
+                        {
+  name: {value: 'Jane'},
+  yearOfBirth: {value: 1969},
+  job: {value: 'designer'}
+});
+
