c5ad34c39a4dec1c945d5517c80275ad9dd075b8
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-03 13:05:11 -0500

    functions returning functions

--- Advanced JS/script.js
+++ Advanced JS/script.js
@@ -121,8 +121,34 @@ console.log(ages);
 console.log(fullAges);
 console.log(rates);
 
+// First Class Functions: Functions returning functions
+function interviewQuestion(job) {
+  if (job === 'designer'){
+    return function(name) {
+      // anonymous fn
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
 
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
 
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
 
 
 
