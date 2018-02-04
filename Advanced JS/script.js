6f78317ae73120c2afa8b2cc33794aac686056d8
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-04 16:41:59 -0500

    closures

--- Advanced JS/script.js
+++ Advanced JS/script.js
@@ -162,12 +162,29 @@ game();
   console.log(score >= 5);
 })();
 
-console.log(score);
+// console.log(score);
 
 (function (goodLuck) {
   var score = Math.random() * 10;
   console.log(score >= 5 - goodLuck);
 })(5);
+
+// Closures
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+  return function(yearOfBirth) {
+    var age = 2018 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
+
+retirement(66)(1990);
+
+
+
 
 
 
