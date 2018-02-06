bcf163505f7efd6178c836c41868d32a24ce7ae5
Alyson Stiffel <alysonstiffel@gmail.com>
2018-02-06 13:11:29 -0500

    coding challenge function constructor

--- Advanced JS/Challenge/script.js
+++ Advanced JS/Challenge/script.js
@@ -20,8 +20,16 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
+var Question = function(question, answers, correct) {
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+}
 
-
+// prototype for question to display
+// prototype for checking answers
+// new Question
+// randomize answers display?
 
 /*
 --- Expert level ---
