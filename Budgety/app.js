3cbb40ae6e213301cb14a4c229e970aa8c8bc560
Alyson Paige <alysonstiffel@gmail.com>
2018-03-12 11:07:35 -0400

    adding new item to budget controller

--- Budgety/app.js
+++ Budgety/app.js
@@ -21,7 +21,35 @@ var budgetController = (function() {
       exp: 0,
       inc: 0
     }
-  }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // create new ID
+      if(data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // create new item based on type, inc or exp
+      if(type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // push it into data structure
+      data.allItems[type].push(newItem);
+      // return new element
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
 
 })();
 
@@ -67,9 +95,11 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
     // 1. Get input data
-    var input = UICtrl.getinput();
+    input = UICtrl.getinput();
     // 2. Add item to budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add item to UI
     // 4. Calculate budget
     // 5. Display budget (UI)
