dbf37926f213ac910976d3cd5debda9f8081332c
Alyson Paige <alysonstiffel@gmail.com>
2018-03-16 12:57:56 -0400

    UI controller

--- Budgety/app.js
+++ Budgety/app.js
@@ -95,8 +95,13 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   }
+
   return {
     getinput: function() {
       return {
@@ -138,6 +143,18 @@ var UIController = (function() {
 
       fieldsArr[0].focus();
     },
+
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      if(obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
     
     // expose DOM strings to public
     getDOMstrings: function() {
@@ -168,7 +185,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 2. Return the budget
     var budget = budgetCtrl.getBudget();
     // 3. Display budget (UI)
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function() {
@@ -191,6 +208,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('Application has started');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
