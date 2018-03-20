8afbc4cfcee3db60beab21572877707867e4ff31
Alyson Paige <alysonstiffel@gmail.com>
2018-03-20 11:00:42 -0400

    let and const

--- ES6/script.js
+++ ES6/script.js
@@ -1,1 +1,58 @@
 // Variable declarations `let` and `const`
+
+//ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+//ES6
+// const: constant, cannot change
+const name6 = 'Jane Smith';
+// let: value of variable can change
+let age6 = 23;
+name6 = 'Jane Miller';
+console.log(name6);
+// error, cannot assign new value to const
+
+// ES5
+function driversLicense5(passedTest) {
+  if(passedTest) {
+    console.log(firstName); // hoisting
+    var firstName = 'John';
+    var yearOfBirth = 1990;
+  }
+  console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicense5(true);
+
+// ES6
+function driversLicense6(passedTest) {
+  // console.log(firstName);
+  let firstName;
+  const yearOfBirth = 1990;
+
+  if(passedTest) {
+    firstName = 'John';
+  }
+  // function is block scoped, can't access variables
+  // need to be within the `if` statement
+  console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicense6(true);
+
+var i = 23;
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log(i);
+// 0, 1, 2, 3, 4, 5
+
+let i = 23;
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log(i);
+// 0, 1, 2, 3, 4, 23
