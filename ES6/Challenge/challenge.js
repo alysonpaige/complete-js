0c785881fa0548b294cc497b9e03b55ff73263cc
Alyson Paige <alysonstiffel@gmail.com>
2018-03-25 10:00:20 -0400

    park class

--- ES6/Challenge/challenge.js
+++ ES6/Challenge/challenge.js
@@ -30,11 +30,21 @@ class Element {
 }
 
 class Park extends Element {
-
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear);
+    this.area = area;
+    this.numTrees = numTrees;
+  }
+  treeDensity() {
+    const density = this.numTrees / this.area;
+    console.log(`${this.name} has a tree density of ${density} trees per square km.`);
+  }
 }
 
 class Street extends Element {
+  constructor(name, buildYear, length, size) {
 
+  }
 }
 
 const allParks = [];
