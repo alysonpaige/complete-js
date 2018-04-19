7cdad9eba1b1412f68a436fbaabbca926aeae113
Alyson Paige <alysonstiffel@gmail.com>
2018-04-19 17:27:43 -0400

    api recipe setup

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -1,1 +1,15 @@
 // Global app controller
+import axios from 'axios';
+
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const key = 'ff7d8eb38d812fcc1a2b8b783377898e';
+  try {
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+getResults('pizza');
