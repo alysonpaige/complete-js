c8c09c83aa57557d60f8bcd44a5ece471367c323
Alyson Paige <alysonstiffel@gmail.com>
2018-04-26 20:05:19 -0400

    building recipe view

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -61,7 +61,8 @@ const controlRecipe = async () => {
 
   if (id) {
     // Prepare UI for changes
-
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
     // Create new recipe object
     state.recipe = new Recipe(id);
 
@@ -76,7 +77,9 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
 
       // Render recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+
     } catch (err) {
       alert('Error processing recipe');
     }
